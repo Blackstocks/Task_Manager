b2c3cc91c6430bdcbcff93b2dfa759484e942e42
AlgoAce2107 <ankitsoni2107.iitkgp@gmail.com>
2024-05-09 00:44:07 +0530

    Initial commit

---
+++ client/src/pages/TaskDetails.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const TaskDetails = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TaskDetails
---
+++ client/src/pages/Thrash.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Thrash = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Thrash
